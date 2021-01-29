f1177617330b7504a0515caa90bcb04ea9a92f4e
victorine <victorine.maurel@hotmail.fr>
2021-01-29 18:41:00 +0100

    interface integration

--- src/webgl/objects/cube.js
+++ src/webgl/objects/cube.js
@@ -2,7 +2,7 @@ import { BoxGeometry, MeshPhysicalMaterial, Mesh } from 'three'
 
 export default class Cube extends Mesh {
 
-  constructor(x,y,z,r,c) {
+  constructor(x,y,z,c) {
     const geometry = new BoxGeometry( 5,5,5);
     const material = new MeshPhysicalMaterial( { color: c, wireframe: false } );
     super(geometry, material);
@@ -10,7 +10,7 @@ export default class Cube extends Mesh {
     this.position.x = x;
     this.position.y = y;
     this.position.z = z;
-    this.rotation.x = r;
+    //this.rotation.x = r;
   }
 
   update() {
